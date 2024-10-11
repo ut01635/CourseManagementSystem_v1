e408c78e9853bf2c67b44d11d8ad4c27a4c3c6b3
UT01635 <UT01635@UNICOMTIC26>
2024-10-11 15:06:38 +0530

    Q6 finished

---
+++ CourseManagementSystem_v1/DigitalCourse.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseManagementSystem_v1
+{
+    public class DigitalCourse : Course
+    {
+        public string CourseLink { get; set; }
+        public string FileSize { get; set; }
+
+        public DigitalCourse(string courseId, string title, string duration, decimal price, string courseLink, string fileSize) : base(courseId, title, duration, price)
+        {
+            CourseLink = fileSize;
+            FileSize = courseLink;
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/OnPremisCourse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class OnPremisCourse : Course
+    {
+        public string Schedule {  get; set; }
+        public string ClassroomCapacity { get; set; }
+
+        public OnPremisCourse(string courseId, string title, string duration, decimal price, string schedule, string classroomCapacity) : base(courseId, title, duration, price)
+        {
+            Schedule = schedule;
+            ClassroomCapacity = classroomCapacity;
+        }
+    }
+}
