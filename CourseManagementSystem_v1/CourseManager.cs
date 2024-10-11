caccc8cf0dfe734e0eb5b381c9df8f1ba8085f75
UT01635 <UT01635@UNICOMTIC26>
2024-10-11 14:57:56 +0530

    Q4 and Q5 Finished

--- CourseManagementSystem_v1/CourseManager.cs
+++ CourseManagementSystem_v1/CourseManager.cs
@@ -9,45 +9,87 @@ namespace CourseManagementSystem_v1
     internal class CourseManager
     {
         public List<Course> courses = new List<Course>();
-       
 
 
-        public Course CreateCourse(Course course)
+
+        public void CreateCourse(string id ,string title, string Duration, decimal Price)
         {
-            Course newCourse = (course);
-            courses.Add(newCourse);
-            Console.WriteLine("Course Created successfully");
-            return newCourse;
+
+
+            Course course = new Course(id, title, Duration, Price);
+            courses.Add(course);
+            Console.WriteLine("Course added successfully");
+
         }
 
-        public void ReadCourses()
+        public void ReadAllCourses()
         {
-            if( courses.Count == 0)
+            if (courses.Count == 0)
             {
-                Console.WriteLine("course Not found");
+                Console.WriteLine("No Courses Available here");
             }
             else
             {
-                Console.WriteLine("---------Available Courses-----------");
-                foreach(var course in courses)
+                Console.WriteLine("--------Available Courses-------");
+                foreach (var course in courses)
                 {
                     Console.WriteLine(course);
                 }
             }
+
         }
 
-        public Course UpdateCourse (string id,Course course)
+        public void UpdateCourse(string courseId, string title, string duration, decimal price)
         {
-            Course newCourse = courses.Find(x =>
-            courses.Remove(course);
-            Console.WriteLine("course updated successfully");
-            return course;
+
+
+            if (courseId != null)
+            {
+                var course = courses.FirstOrDefault(a => a.courseId == courseId);
+                if (course != null)
+                {
+                    course.Title = title;
+                    course.Duration = duration;
+                    course.Price = price;
+                    Console.WriteLine("Program updated successfully");
+                }
+                else
+                {
+                    Console.WriteLine("Course not found");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid Course Id");
+            }
+
+
         }
 
-        public void DeleteCourse(Course course)
+        public void DeleteCourse(string Id)
         {
-            c
-        } 
+
+            if (Id != null)
+            {
+                var course = courses.FirstOrDefault(c => c.CourseId == Id);
+                if (course != null)
+                {
+                    courses.Remove(course);
+                    Console.WriteLine("Course deleted successfully. ");
+
+                }
+                else
+                {
+                    Console.WriteLine("Course not found. ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Course Id");
+            }
+
+        }
 
 
 
--- CourseManagementSystem_v1/Program.cs
+++ CourseManagementSystem_v1/Program.cs
@@ -6,10 +6,139 @@ using System.Threading.Tasks;
 
 namespace CourseManagementSystem_v1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            CourseManager manager = new CourseManager();
+            int choice;
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Course Management System: ");
+                Console.WriteLine("1. Add a Course");
+                Console.WriteLine("2. View All Courses");
+                Console.WriteLine("3. Update a Course");
+                Console.WriteLine("4. Delete a Course");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option:");
+
+
+
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Clear();
+                            CreateCourse();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ReadCourses();
+
+                            break;
+                        case 3:
+                            Console.Clear();
+                            UpdateCourse(manager);
+
+
+                            break;
+                        case 4:
+                            Console.Clear();
+                            DeleteCourse(manager);
+
+                            break;
+                        case 5:
+                            Console.Clear();
+                            Console.WriteLine("Exiting the system. good bye");
+                            Console.WriteLine("Press any key to exit");
+                            Console.ReadKey();
+                            break;
+                        default:
+                            //Console.Clear();
+                            Console.WriteLine("Invalid opion please try again");
+                            break;
+                    }
+
+                    if (choice != 5)
+                    {
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            } while (choice != 5);
+
+
+
+
+            ////////////////////////////////////Methods/////////////////////////////////////
+
+            void CreateCourse()
+            {
+                Console.Write("Enter Course ID: ");
+                string id = Console.ReadLine();
+
+                Console.Write("Enter Course Title: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Enter Course Duration: ");
+                string Duration = Console.ReadLine();
+
+                Console.Write("Enter Course Price: ");
+                decimal Price = decimal.Parse(Console.ReadLine());
+
+                manager.CreateCourse(id, title, Duration, Price);
+            }
+
+            
+
+             void UpdateCourse(CourseManager manager1)
+            {
+                Console.Write("Enter Course ID to update: "); ;
+                string id = Console.ReadLine();
+
+                Console.Write("Enter new Title(leave blank to keep current): ");
+                string title = Console.ReadLine();
+
+                Console.Write("Enter new Duration(leave blank to keep current): ");
+                string duration = Console.ReadLine();
+
+               Console.WriteLine("EnEnter new Price(leave blank to keep current):");
+                decimal priceInput = decimal.Parse(Console.ReadLine());
+
+
+                manager1.(id, title, duration, priceInput);
+            }
+
+
+             void DeleteCourse(CourseManager manager1)
+            {
+                Console.WriteLine("Enter course ID  to delete:");
+                int id = int.Parse(Console.ReadLine());
+
+                //manager.DeleteCourse(id);
+                
+            }
+
+             void ReadCourses()
+            {
+
+                manager.ReadAllCourses();
+                
+            }
+
+
+
         }
+
     }
 }
