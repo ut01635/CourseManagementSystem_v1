a2ba34b34af98c1557ba53175aa2ee380f9eac88
UT01635 <UT01635@UNICOMTIC26>
2024-10-11 14:16:25 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    public class Course
+    {
+         private string courseId {  get; set; }
+         private string title { get; set; }
+         private string Duration { get; set; }
+         private decimal Price { get; set; }
+
+        public Course(string courseId, string title, string duration, decimal price)
+        {
+            this.courseId = courseId;
+            this.title = title;
+             Duration = duration;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {courseId}, Title: {title}, Duration: {Duration}, Price: {Price}";
+        }
+
+        //Course course1 = {  courseId: "C_001", 
+        //                    title: "python for beginners", 
+        //                    Duration: "3 months", 
+        //                    Price: 10.00 
+        //                  }
+    }
+}
---
+++ CourseManagementSystem_v1/CourseManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class CourseManager
+    {
+        public List<Course> courses = new List<Course>();
+       
+
+
+        public Course CreateCourse(Course course)
+        {
+            Course newCourse = (course);
+            courses.Add(newCourse);
+            Console.WriteLine("Course Created successfully");
+            return newCourse;
+        }
+
+        public void ReadCourses()
+        {
+            if( courses.Count == 0)
+            {
+                Console.WriteLine("course Not found");
+            }
+            else
+            {
+                Console.WriteLine("---------Available Courses-----------");
+                foreach(var course in courses)
+                {
+                    Console.WriteLine(course);
+                }
+            }
+        }
+
+        public Course UpdateCourse (string id,Course course)
+        {
+            Course newCourse = courses.Find(x =>
+            courses.Remove(course);
+            Console.WriteLine("course updated successfully");
+            return course;
+        }
+
+        public void DeleteCourse(Course course)
+        {
+            c
+        } 
+
+
+
+    }
+}
